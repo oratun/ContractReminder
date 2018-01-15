d3a324e267978e9cd42fc1e715c0bdaf510325b0
ora-ws <warn1667@gmail.com>
2018-01-15 15:57:38 +0800

    readme.md update

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ from app.models import User, Follow, Role, Permission, Post
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
@@ -15,14 +16,6 @@ def make_shell_context():
                 Permission=Permission, Post=Post)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
-
-
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 if __name__ == '__main__':
